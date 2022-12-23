3ff5acafde8c2ca1f56b64f55935fb7033bc5d2c
MartinCalcaterra <martin.calcaterra@outlook.com>
2022-12-23 17:49:07 -0500

    Configured CMake file; OpenGL demo working for MartinDemo

--- MartinDemo/main.c
+++ MartinDemo/main.c
@@ -1,6 +1,53 @@
-#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main() {
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+/*#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GLFW_TRUE);
+#endif*/
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(600, 400, "Demo", NULL, NULL);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create window\n");
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("Failed to initialize GLAD\n");
+        return 1;
+    }
+
+    //glViewport(0, 0, 800, 600);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        //color of the window
+        glClearColor(0.0, 1.0, 1.0, 0.0);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     return 0;
 }
