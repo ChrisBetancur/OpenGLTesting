57135e48f56283bf6f1df481503f5c1672cc5e2e
MartinCalcaterra <martin.calcaterra@outlook.com>
2022-12-29 23:05:49 -0500

    Fixed triangle.c for 5.3. Now works, no segfaults

--- MartinDemo/chapters/triangle.c
+++ MartinDemo/chapters/triangle.c
@@ -1,5 +1,9 @@
 //
 // Created by marti on 2022-12-27.
+
+// based on chapter 5 of the textbook
+
+// note i made up: triangles are the building blocks of graphics because they are the most elementary polygon
 //
 
 #include <glad/glad.h>
@@ -10,7 +14,8 @@
 
 // copy and pasted directly from pdf of textbook
 // simplest possible vertex shader; takes in input and directly sends it to the next shader
-const char *vertexShaderSource = "#version 330 core\n"
+// C context: remember that * is for pointers. In this case, a char pointer works as a string (read more to understand)
+static const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
                                  "void main()\n"
                                  "{\n"
@@ -20,7 +25,7 @@ const char *vertexShaderSource = "#version 330 core\n"
 static int triangle() {
 
     // evidently using every 3 points to be a different vertex (no 2D array)
-    float vertices[] = {
+    float vertices[9] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f
@@ -31,6 +36,7 @@ static int triangle() {
 
     // generate a vertex buffer object, our first OpenGL object. Give buffer ID.
     unsigned int VBO;
+    // 1 means we are only giving an id for 1 buffer object (our VBO is a single int, not an array)
     glGenBuffers(1, &VBO);
 
     // we can bind our vbo to a buffer target (a buffer within the GPU):
@@ -55,7 +61,49 @@ static int triangle() {
     // create vertex shader:
 
     // we can get a glsl plugin for CLion and make a triangle.glsl file, but for now we
-    // can just compile the source code as a constant string above.
+    // can just dynamically compile the source code (which is stored as a constant above) at runtime:
+
+    // variable for shader
+    unsigned int vertexShader;
+    // create a shader with the GL_VERTEX_SHADER id
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // attach the shader source code to our shader
+    // first 3 arguments: shader object to compile, number of source code strings, and source code for the shader
+    // 4th param is NULL for now
+    // C note: read about what the & does
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // compile the shader
+    glCompileShader(vertexShader);
+
+
+    // check for compilation errors: (self note: should i be using helper functions for these things???)
+
+    // an int (1 or 0) for whether compilation succeeded
+    int success;
+    // a string for the info log (error info if there was failure)
+    char infoLog[512];
+    // store whether the compilation succeeded in the success variable:
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    // if there was not a success, get and print the info log:
+    if (!success) {
+        // get the error info
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        // print it (using printf because C doesn't have cout)
+        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
+
+    }
+
+
+    // running this for the first time, i get error 0xC0000005 for accessing memory i have no permission to access
+    // first investigation: infoLog array
+    // using commenting, looks like the error comes from above the compile checking
+
+    // i learned address 0x0 is the null pointer (i think)
+
+    // different things happen if i call triangle() after createWindow()
+    // the infoLog turns out to be garbage
 
 
     return 0;
--- MartinDemo/main.c
+++ MartinDemo/main.c
@@ -3,6 +3,8 @@
  * Made by Martin Calcaterra, December 23rd, 2022.
  *
  * Using Learn OpenGL book by Joey de Vries so far
+ *
+ * Note i am also learning more about C at the same time, so I am leaving notes about C specifically through my work
  * */
 
 // < and > are for static libraries
@@ -13,12 +15,25 @@
 #include <stdlib.h>
 
 // quotations are for other source files
-// include the chapter 1 code
+// include the chapter 1 code (its function is static)
 #include "chapters/window.c"
+#include "chapters/triangle.c"
+
+const static char *endmsg = "End of program";
 
 int main()
 {
+    // this seems to have to happen before we create a triangle
     createWindow();
+
+    triangle();
+
+    printf("\n%s", endmsg);
+
+    // errors have something to do with init and terminating glfw at the wrong times; maybe earlier textbook chapters
+    // may help
+    // i'm pretty sure right now, i'm creating a vbo after closing a window, but before terminating glfw
+    glfwTerminate();
 
     return 0;
 }
